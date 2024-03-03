261fd61bca9c62270ed08b315adca0c051553f16
Marc Baumgartner <marc@mabaka.ch>
2024-03-03 17:37:29 +0100

    Add a user menu with logout command

--- src/main/java/ch/mabaka/manualtestmanager/view/app/App.java
+++ src/main/java/ch/mabaka/manualtestmanager/view/app/App.java
@@ -18,15 +18,34 @@ package ch.mabaka.manualtestmanager.view.app;
 import java.io.Serializable;
 import java.util.Locale;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
 import ch.mabaka.manualtestmanager.domain.Country;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Named
 @SessionScoped
 public class App implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	Logger logger;
+	
+	@Inject
+    private FacesContext facesContext;
+	
+	@ConfigProperty(name = "quarkus.http.auth.form.login-page")
+	String loginPage;
+	
 	private String theme = "saga";
     private boolean darkMode = false;
     private String inputStyle = "outlined";
@@ -103,5 +122,32 @@ public class App implements Serializable {
 			break;
 		}
     	return result;
+    }
+    
+    public String logout() {
+        final String result = loginPage;
+        
+        HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+        HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+        
+        try {
+            Cookie[] cookies = request.getCookies();
+            for (Cookie c : cookies) {
+                System.out.println(c.getName());
+                if (c.getName().equals("quarkus-credential")) {
+                    System.out.println("Deleting cookie");
+                    c.setPath("/");
+                    c.setMaxAge(0);
+                    response.addCookie(c);
+                    break;
+                }
+            }
+            request.logout();
+            facesContext.getExternalContext().invalidateSession();
+        } catch (ServletException e) {
+        	logger.warn("Exception during logout:", e);
+        }
+        
+        return result;
     }
 }
